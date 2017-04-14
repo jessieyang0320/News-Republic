cb4c0d61d4d55ff86c181ff1ee035fee3583a8a1
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-04-14 13:53:43 -0400

    add header and user registration to mobile page

--- src/js/components/mobile_header.js
+++ src/js/components/mobile_header.js
@@ -1,16 +1,113 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Row, Col} from 'antd';
+import {Menu,Icon,Tabs,message,Form,Input,Button,CheckBox,Modal} from 'antd';
+const FormItem = Form.Item;
+const SubMenu = Menu.SubMenu;
+const TabPane = Tabs.TabPane;
+const MenuItemGroup = Menu.ItemGroup;
 
-export default class MobileHeader extends React.Component{
+
+class MobileHeader extends React.Component{
+	constructor() {
+		super();
+		this.state = {
+			current: 'top',
+			modalVisible: false,
+			action: 'login',
+			hasLogined: false,
+			userNickName: '',
+			userid: 0
+		};
+	};
+
+	setModalVisible(value)
+	{
+		this.setState({modalVisible: value});
+	};
+
+	handleClick(e) {
+		console.log(e.key);
+		if (e.key == "register") {
+			this.setState({current: 'register'});
+			this.setModalVisible(true);
+		} else {
+			{
+				this.setState({current: e.key});
+			}
+		}
+	};
+
+	handleSubmit(e)
+	{
+		//页面开始向 API 进行提交数据
+		e.preventDefault();
+		var myFetchOptions = {
+			method: 'GET'
+		};
+		var formData= this.props.form.getFieldsValue();
+		console.log(formData);
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName="+formData.r_userName+"&r_password="+formData.r_password+"&r_confirmPassword="+formData.r_confirmPassword,myFetchOptions).
+		then(response=>response.json()).then(json=>{
+			this.setState({userNickName:json.NickUserName,userid:json.UserId});
+
+		});
+		message.success("请求成功！");
+		this.setModalVisible(false);
+	};
+
+
+
+login(){
+	this.setModalVisible(true);
+}
+
 	render(){
+		let {getFieldDecorator} = this.props.form;
+		const userShow = this.state.hasLogined ?
+				<Link>
+					<Icon type="inbox"/>
+				</Link>
+				:
+				<Icon type="setting" onClick={this.login.bind(this)}/>
+
 		return(
 		  <div id="mobileheader">
 		    <header>
 		      <img src="./src/images/logo.png" alt="logo"/>
 		      <span>ReactNews</span>
+					{userShow}
 		    </header>
+
+				<Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText = "关闭">
+					<Tabs type="card">
+						<TabPane tab="注册" key="2">
+							<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+								<FormItem label="账户">
+									{getFieldDecorator('r_userName')(
+										<Input placeholder="请输入您的账号"/>
+									)}；
+								</FormItem>
+
+								<FormItem label="密码">
+									{getFieldDecorator('r_password')(
+										<Input placeholder="请输入您的密码"/>
+									)}；
+								</FormItem>
+
+								<FormItem label="确认密码">
+									{getFieldDecorator('r_confirmPassword')(
+										<Input placeholder="请确认您的密码"/>
+									)}；
+								</FormItem>
+								<Button type="primary" htmlType="submit" >注册</Button>
+							</Form>
+						</TabPane>
+					</Tabs>
+				</Modal>
 		  </div>
 
 			);
 	}
 }
+export default MobileHeader = Form.create({})(MobileHeader);
