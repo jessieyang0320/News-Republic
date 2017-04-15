fbebc5a59c4bfc37810f6541560b8c49da288e9b
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-04-15 15:50:23 -0400

    add user comments list to user center on both mobile and pc page

--- src/js/components/mobile_usercenter.js
+++ src/js/components/mobile_usercenter.js
@@ -14,6 +14,7 @@ export default class MobileUserCenter extends React.Component{
     super();
     this.state={
       usercollection:'',
+      usercomments: '',
       previewImage: '',
       previewVisible: false
     }
@@ -28,6 +29,12 @@ export default class MobileUserCenter extends React.Component{
     .then(response=>response.json())
     .then(json=>{this.setState({usercollection:json})
   });
+
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid="
+    + localStorage.userid, myFetchOptions)
+    .then(response=>response.json())
+    .then(json=>{this.setState({usercomments:json})
+     });
   }
 
 
@@ -52,7 +59,7 @@ export default class MobileUserCenter extends React.Component{
     	}
    };
 
-   const {usercollection} = this.state;
+   const {usercollection,usercomments} = this.state;
    const usercollectionList = usercollection.length ?
    usercollection.map((uc,index)=>(
        <Card key={index} title={uc.uniquekey} extra={<a target="_blank" href={`/#/details/${uc.uniquekey}`}>查看</a>}>
@@ -61,6 +68,16 @@ export default class MobileUserCenter extends React.Component{
    ))
    :
    '您还没有收藏任何的新闻，快去收藏一些新闻吧。';
+
+   const usercommentsList = usercomments.length ?
+   usercomments.map((comment,index)=>(
+       <Card key={index} title={`于 ${comment.datetime} 评论了文章 ${comment.uniquekey}`}
+       extra={<a target="_blank" href={`/#/details/${comment.uniquekey}`}>查看</a>}>
+         <p>{comment.Comments}</p>
+       </Card>
+   ))
+   :
+   '您还没有发表过任何评论';
 
     return(
       <div>
@@ -78,6 +95,12 @@ export default class MobileUserCenter extends React.Component{
                   </Row>
                 </TabPane>
                 <TabPane tab="我的评论列表" key="2">
+                  <Row>
+                      <Col span={24}>
+                          {usercommentsList}
+                      </Col>
+
+                  </Row>
                 </TabPane>
                 <TabPane tab="头像设置" key="3">
                     <div className="clearfix">
--- src/js/components/pc_usercenter.js
+++ src/js/components/pc_usercenter.js
@@ -14,6 +14,7 @@ export default class PCUserCenter extends React.Component{
     super();
     this.state={
       usercollection:'',
+      usercomments: '',
       previewImage: '',
       previewVisible: false
     }
@@ -23,11 +24,19 @@ export default class PCUserCenter extends React.Component{
     var myFetchOptions = {
       method: 'GET'
     };
+
     fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid="
     + localStorage.userid, myFetchOptions)
     .then(response=>response.json())
     .then(json=>{this.setState({usercollection:json})
-  });
+     });
+
+     fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid="
+     + localStorage.userid, myFetchOptions)
+     .then(response=>response.json())
+     .then(json=>{this.setState({usercomments:json})
+      });
+
   }
 
 
@@ -52,7 +61,7 @@ export default class PCUserCenter extends React.Component{
     	}
    };
 
-   const {usercollection} = this.state;
+   const {usercollection, usercomments} = this.state;
    const usercollectionList = usercollection.length ?
    usercollection.map((uc,index)=>(
        <Card key={index} title={uc.uniquekey} extra={<a target="_blank" href={`/#/details/${uc.uniquekey}`}>查看</a>}>
@@ -61,6 +70,18 @@ export default class PCUserCenter extends React.Component{
    ))
    :
    '您还没有收藏任何的新闻，快去收藏一些新闻吧。';
+
+   const usercommentsList = usercomments.length ?
+   usercomments.map((comment,index)=>(
+       <Card key={index} title={`于 ${comment.datetime} 评论了文章 ${comment.uniquekey}`}
+       extra={<a target="_blank" href={`/#/details/${comment.uniquekey}`}>查看</a>}>
+         <p>{comment.Comments}</p>
+       </Card>
+   ))
+   :
+   '您还没有发表过任何评论';
+
+
 
 
     return(
@@ -80,6 +101,12 @@ export default class PCUserCenter extends React.Component{
 
                 </TabPane>
                 <TabPane tab="我的评论列表" key="2">
+                    <Row>
+                        <Col span={24}>
+                            {usercommentsList}
+                        </Col>
+
+                    </Row>
                 </TabPane>
                 <TabPane tab="头像设置" key="3">
                     <div className="clearfix">
