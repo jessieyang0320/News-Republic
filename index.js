d09df5b18e31cbd87c367452c558fdf542185d0b
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-05-01 20:42:27 -0400

    change render page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require("http"),
  
 var server = http.createServer(function(request,response){  
     response.writeHeader(200, {"Content-Type": "text/plain"});  
-    response.write("Hello HTTP!");  
+    response.render('index'); 
     response.end();  
 });
  
