28185b3b800c414ca7e9172c93eeee12a1b04cb3
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-05-01 20:28:40 -0400

    add index.js and procfile for deploy on heroku

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require("http"),
+    port = process.env.PORT || 1881;  
+ 
+var server = http.createServer(function(request,response){  
+    response.writeHeader(200, {"Content-Type": "text/plain"});  
+    response.write("Hello HTTP!");  
+    response.end();  
+});
+ 
+server.listen(port);  
+console.log("Server Running on "+port+".\nLaunch http://localhost:"+port);
