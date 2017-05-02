0840dd604ed287b247cacf18631ec977f482648d
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-05-01 21:23:57 -0400

    change server file

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
