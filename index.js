3fec450e5c3c43fcdf59ff5cba666f1bde8da8f6
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-05-01 21:21:12 -0400

    change server file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 // app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
